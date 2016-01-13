ed8fabc928d8bfcfcd584651f9c9211ed0fa8de4
欧瑞强 <ouruiqiang@ucfgroup.com>
2016-01-13 11:23:24 +0800

    sdad

--- src/com/topad/util/Constants.java
+++ src/com/topad/util/Constants.java
@@ -43,21 +43,14 @@ public class Constants {
      * 更新其它图片数据
      */
     public static final String URL_UPDATE_IMG = "/user/updateimg";
-<<<<<<< HEAD
     /**
      * 媒体搜索
      */
     public static final String URL_MEDIA_SEARCH = "/media/search";
 
 
-
-
     /*********
      * 下载图片拼接*/
-=======
-
->>>>>>> 9b0a8099b0e15f4a2098037bb96603fe5ad55171
-    // 下载图片拼接
     public static final String IMAGE_URL_HEADER = "/userimg/";
     // 添加产品
     public static final String URL_ADD_PRODUCT= "/adservice/add";
