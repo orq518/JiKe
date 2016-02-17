86fa776cfc58c3dcc43c811fa83276328d4b4357
欧瑞强 <ouruiqiang@ucfgroup.com>
2016-02-17 16:29:20 +0800

    21313

--- src/com/topad/view/activity/ShareNeedsEditActivity_Normal.java
+++ src/com/topad/view/activity/ShareNeedsEditActivity_Normal.java
@@ -13,6 +13,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
@@ -241,6 +242,8 @@ public class ShareNeedsEditActivity_Normal extends BaseActivity implements IReco
                 recordTools.showVoiceDialog();
                 break;
             case R.id.data_pic:
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
                 PickDatePopwindow datePick = new PickDatePopwindow(ShareNeedsEditActivity_Normal.this);
                 datePick.registeDatePick(this);
                 datePick.showAtLocation(bottom_layout,
--- src/com/topad/view/activity/ShareNeedsEditActivity_Peixun.java
+++ src/com/topad/view/activity/ShareNeedsEditActivity_Peixun.java
@@ -10,6 +10,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
@@ -197,6 +198,8 @@ public class ShareNeedsEditActivity_Peixun extends BaseActivity implements IReco
                 recordTools.showVoiceDialog();
                 break;
             case R.id.data_pic:
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
                 PickDatePopwindow datePick = new PickDatePopwindow(ShareNeedsEditActivity_Peixun.this);
                 datePick.registeDatePick(this);
                 datePick.showAtLocation(mainlayout,
