295237757b7c15a914485ecc99aa299a91aa2733
欧瑞强 <ouruiqiang@ucfgroup.com>
2016-02-23 17:12:59 +0800

    afsasf

--- src/com/topad/amap/ToastUtil.java
+++ src/com/topad/amap/ToastUtil.java
@@ -6,9 +6,14 @@ package com.topad.amap;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.topad.util.Utils;
+
 public class ToastUtil {
 
 	public static void show(Context context, String info) {
+		if(Utils.isEmpty(info)){
+			return;
+		}
 		Toast.makeText(context, info, Toast.LENGTH_LONG).show();
 	}
 
