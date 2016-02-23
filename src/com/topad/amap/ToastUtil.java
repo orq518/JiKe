2f5cb2e05a611c4a92e823ffa00e1122dadbe2a5
欧瑞强 <ouruiqiang@ucfgroup.com>
2016-02-23 18:19:21 +0800

    sdss

--- src/com/topad/amap/ToastUtil.java
+++ src/com/topad/amap/ToastUtil.java
@@ -11,7 +11,7 @@ import com.topad.util.Utils;
 public class ToastUtil {
 
 	public static void show(Context context, String info) {
-		if(Utils.isEmpty(info)){
+		if(Utils.isEmpty(info)||context==null){
 			return;
 		}
 		Toast.makeText(context, info, Toast.LENGTH_LONG).show();
